27690adeeadb439cfe513c27678d09730cea2cad
narabala <71353877+narabala@users.noreply.github.com>
2020-09-22 16:59:12 -0400

    Week1

---
+++ NaraWebApp/NaraWebApp/Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NaraWebApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "This is the default action";
+        }
+        public string ManageTask(string task, int ID)
+        {
+            return $"This is the task manager {task} is important with this {ID}";
+        }
+    }
+
+}

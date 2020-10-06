742f7bd8d04d48535fd00cdc8abbd7f7fffc6d3e
narabala <71353877+narabala@users.noreply.github.com>
2020-10-06 16:54:37 -0400

    anotherDay

---
+++ NaraWebApp/NaraWebApp/Data/NTaskContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace NaraWebApp.Data
+{
+    public class NTaskContext :DbContext
+    {
+        public NTaskContext()
+        {
+        }
+    }
+}
---
+++ NaraWebApp/NaraWebApp/Models/NTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NaraWebApp.Models
+{
+    [Table("task")]
+    public class NTask
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedBy { get; set; }
+
+    }
+
+}
